d158b704cd75c6bb9c8f280c5c69ba68706c89d3
Rashed Anowar <rashed.anowar@bol-online.com>
2023-02-23 12:15:54 +0600

    code updte

---
+++ plugins/bol/eshop/controllers/Carts.php
@@ -0,0 +1,22 @@
+<?php namespace Bol\Eshop\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Carts extends Controller
+{
+    public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController'    ];
+    
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    public $requiredPermissions = [
+        'bol.eshop.manage_carts' 
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Bol.Eshop', 'main-menu-item', 'side-menu-item14');
+    }
+}
